5a861b731fc9abf004e49a9c75ec828427934bec
Krasny-Lis <akceleratorr@gmail.com>
2025-12-19 16:44:11 +0100

    Move game timers into NgRx effects

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { bootstrapApplication } from "@angular/platform-browser";
+import { provideEffects } from "@ngrx/effects";
 import { provideStore } from "@ngrx/store";
 import { AppComponent } from "./app/app.component";
+import { GameEffects } from "./app/store/effects/game.effects";
 import { gameReducer } from "./app/store/game.reducer";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideStore({ game: gameReducer })],
+  providers: [provideStore({ game: gameReducer }), provideEffects(GameEffects)],
 }).catch((err) => console.error(err));
